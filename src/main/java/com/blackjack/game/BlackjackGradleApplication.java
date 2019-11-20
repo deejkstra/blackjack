3d184807b7aca65ddbded72ca1f572a354e06f0a
Matt <mjkrupin@gmail.com>
2019-11-20 17:24:03 -0500

    Initial Commit; Setting up Gradle.

---
+++ src/main/java/com/blackjack/game/BlackjackGradleApplication.java
@@ -0,0 +1,13 @@
+package com.blackjack.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackjackGradleApplication.class, args);
+	}
+
+}
